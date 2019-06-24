896c66efdd825bc4587274ff340a1754b03947be
Richard Borges <richardborgescc@users.noreply.github.com>
2019-06-24 12:12:25 -0300

    Information Security And Quality Assurance. Mochachai: Testing http response.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -119,14 +119,18 @@ suite('Functional Tests', function() {
        
        // we setup the request for you...
        chai.request(server)
-        .put('/travellers')
+                .put('/travellers')
         /** send {surname: 'Colombo'} here **/
         // .send({...})
+        .send({surname: 'Colombo'}) 
         .end(function(err, res){
           
           /** your tests here **/
-          assert.fail(); // remove this after adding tests
-          
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', "Response should be json");
+            
+          assert.equal(res.body.name, 'Cristoforo', 'res.body.name should be "Cristoforo"');
+          assert.equal(res.body.surname, 'Colombo', 'res.body.surname should be "Colombo"');
           done(); // Never forget the 'done()' callback...
         });
       });
